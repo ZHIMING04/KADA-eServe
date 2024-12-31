931dbb5e5e5d77f1b14483a78359fe865653f56f
Pegasus762 <148305613+Pegasus762@users.noreply.github.com>
2024-12-31 13:36:56 +0800

    Normalisation and addition of controller

---
+++ app/Models/Member.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Member extends Model
+{
+    protected $table = 'registered_members';
+    
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'ic',
+        'address',
+        'city',
+        'poskod',
+        'state',
+        'gender',
+        'gred',
+        'salary',
+
+    ];
+}
