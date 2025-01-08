6e0731fa4ce6f26730370809ae193994e51fbc02
Sabrina <sabrinaheng115@gmail.com>
2025-01-08 10:25:06 +0800

    delete web

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/admindashb', function () {
-    return view('admindashb');
-})->middleware(['auth', 'verified'])->name('admindashb');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
