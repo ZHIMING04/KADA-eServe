d66ba79c8b6dd6dfa5d2c1fb4b4b65d0f24d9f41
Pegasus762 <148305613+Pegasus762@users.noreply.github.com>
2025-01-13 21:28:41 +0800

    Update Loan Database

--- database/migrations/2025_01_09_161458_create_member_management_table.php
+++ database/migrations/2025_01_09_161458_create_member_management_table.php
@@ -14,9 +14,8 @@ class CreateMemberManagementTable extends Migration
     public function up(): void
 {
     Schema::create('member_register', function (Blueprint $table) {
-        $table->id();
         $table->timestamps();
-        $table->string('no_anggota');
+        $table->string('no_anggota')->primary();
         $table->string('name');
         $table->string('email');
         $table->string('ic');
@@ -46,8 +45,11 @@ class CreateMemberManagementTable extends Migration
         $table->string('gred');
         $table->string('no_pf');
         $table->string('salary');
-        $table->unsignedBigInteger('no_anggota'); // Add this column
-        $table->foreign('no_anggota')->references('id')->on('member_register')->onDelete('cascade');
+        $table->string('no_anggota');
+        $table->foreign('no_anggota')
+              ->references('no_anggota')
+              ->on('member_register')
+              ->onDelete('cascade');
     });
 
     Schema::create('family', function (Blueprint $table) {
@@ -56,8 +58,11 @@ class CreateMemberManagementTable extends Migration
         $table->string('relationship');
         $table->string('name');
         $table->string('ic');
-        $table->unsignedBigInteger('no_anggota'); // Add this column
-        $table->foreign('no_anggota')->references('id')->on('member_register')->onDelete('cascade');
+        $table->string('no_anggota');
+        $table->foreign('no_anggota')
+              ->references('no_anggota')
+              ->on('member_register')
+              ->onDelete('cascade');
     });
 
     Schema::create('savings', function (Blueprint $table) {
@@ -70,8 +75,11 @@ class CreateMemberManagementTable extends Migration
         $table->decimal('welfare_fund', 10, 2);
         $table->decimal('fixed_savings', 10, 2);
         $table->decimal('total_amount', 10, 2);
-        $table->unsignedBigInteger('no_anggota');
-        $table->foreign('no_anggota')->references('id')->on('member_register')->onDelete('cascade');
+        $table->string('no_anggota');
+        $table->foreign('no_anggota')
+              ->references('no_anggota')
+              ->on('member_register')
+              ->onDelete('cascade');
     });
 }
 
