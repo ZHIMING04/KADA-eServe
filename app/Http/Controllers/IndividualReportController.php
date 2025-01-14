9fb6d46700fd8c10420af12c4d8bd930758d54ed
polok <lokyee1418@gmail.com>
2025-01-14 22:26:22 +0800

    update individual report update the individual report page , hevent done

--- app/Http/Controllers/IndividualReportController.php
+++ app/Http/Controllers/IndividualReportController.php
@@ -3,14 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // Import the Auth facade
 use App\Models\IndividualReport;
+use App\Models\Member;
+use App\Models\Loan;
 
 
 class IndividualReportController extends Controller
 {
     public function display()
     {
-        return view('individualReport.report');
+        //  // Retrieve the authenticated user
+        //  $user = Auth::user();
+
+        //  // Retrieve member details from the members table using the authenticated user's ID
+        //  $member = Member::where('guest_id', $user->id)->first();
+ 
+        //  // Check if the member exists
+        //  if (!$member) {
+        //      return redirect()->back()->with('error', 'Member not found.');
+        //  }
+ 
+        //  // Retrieve loan details for the member
+        //  $loans = Loan::where('no_anggota', $member->no_anggota)->get();
+ // return view('individualReport.report', compact('member', 'loans'));
+         return view('individualReport.report');
     }
 }
 
